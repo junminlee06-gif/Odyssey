1018b0187bcfe037e53bcd5aa05faab2288db039
junminlee06-gif <junminlee06@gmail.com>
2026-06-22 01:53:33 +0900

    Split Odysseus walk and jump animation frames

--- sprite-cropper.js
+++ sprite-cropper.js
@@ -3,11 +3,19 @@
     ['heroIdle','./assets/characters/odysseus_sheet.png',0,40,56],
     ['heroIdleB','./assets/characters/odysseus_sheet.png',1,40,56],
     ['heroWalkA','./assets/characters/odysseus_sheet.png',2,40,56],
-    ['heroWalkC','./assets/characters/odysseus_sheet.png',3,40,56],
-    ['heroWalkB','./assets/characters/odysseus_sheet.png',4,40,56],
+    ['heroWalkB','./assets/characters/odysseus_sheet.png',3,40,56],
+    ['heroWalkC','./assets/characters/odysseus_sheet.png',4,40,56],
     ['heroWalkD','./assets/characters/odysseus_sheet.png',5,40,56],
-    ['heroJump','./assets/characters/odysseus_sheet.png',6,40,56],
-    ['heroFall','./assets/characters/odysseus_sheet.png',7,40,56],
+    ['heroWalkE','./assets/characters/odysseus_sheet.png',6,40,56],
+    ['heroWalkF','./assets/characters/odysseus_sheet.png',7,40,56],
+    ['heroWalkG','./assets/characters/odysseus_sheet.png',8,40,56],
+    ['heroWalkH','./assets/characters/odysseus_sheet.png',9,40,56],
+    ['heroJumpStart','./assets/characters/odysseus_sheet.png',10,40,56],
+    ['heroJumpUp','./assets/characters/odysseus_sheet.png',11,40,56],
+    ['heroJumpApex','./assets/characters/odysseus_sheet.png',12,40,56],
+    ['heroFallA','./assets/characters/odysseus_sheet.png',13,40,56],
+    ['heroLand','./assets/characters/odysseus_sheet.png',14,40,56],
+    ['heroReady','./assets/characters/odysseus_sheet.png',15,40,56],
     ['prop','./assets/characters/npcs_sheet.png',0,48,64],
     ['propB','./assets/characters/npcs_sheet.png',1,48,64],
     ['survivor','./assets/characters/npcs_sheet.png',2,48,64],
@@ -49,6 +57,25 @@
     await imageLoaded(out);
     return out;
   }
+  function animateJumpPose() {
+    try {
+      if (typeof assets !== 'undefined') {
+        if (typeof playerY !== 'undefined' && typeof velocityY !== 'undefined' && playerY > 2) {
+          if (velocityY > 460) assets.heroJump = assets.heroJumpStart || assets.heroJump;
+          else if (velocityY > 130) assets.heroJump = assets.heroJumpUp || assets.heroJump;
+          else if (velocityY > -110) {
+            assets.heroJump = assets.heroJumpApex || assets.heroJump;
+            assets.heroFall = assets.heroJumpApex || assets.heroFall;
+          } else if (velocityY > -470) assets.heroFall = assets.heroFallA || assets.heroFall;
+          else assets.heroFall = assets.heroReady || assets.heroFallA || assets.heroFall;
+        } else {
+          assets.heroJump = assets.heroJumpStart || assets.heroJump;
+          assets.heroFall = assets.heroFallA || assets.heroFall;
+        }
+      }
+    } catch (_) {}
+    requestAnimationFrame(animateJumpPose);
+  }
   async function applyPngSprites() {
     if (typeof assets === 'undefined') return;
     const replacements = await Promise.all(jobs.map(async job => [job[0], await crop(job[1], job[2], job[3], job[4])]));
@@ -59,10 +86,17 @@
     }
     if (typeof heroWalkFrames !== 'undefined') {
       heroWalkFrames[0] = assets.heroWalkA;
-      heroWalkFrames[1] = assets.heroWalkC;
-      heroWalkFrames[2] = assets.heroWalkB;
+      heroWalkFrames[1] = assets.heroWalkB;
+      heroWalkFrames[2] = assets.heroWalkC;
       heroWalkFrames[3] = assets.heroWalkD;
+      heroWalkFrames[4] = assets.heroWalkE;
+      heroWalkFrames[5] = assets.heroWalkF;
+      heroWalkFrames[6] = assets.heroWalkG;
+      heroWalkFrames[7] = assets.heroWalkH;
     }
+    assets.heroJump = assets.heroJumpStart;
+    assets.heroFall = assets.heroFallA;
+    animateJumpPose();
   }
   applyPngSprites();
 })();
