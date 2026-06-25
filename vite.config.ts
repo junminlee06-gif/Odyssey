bbc44f7a02fe5e71e664e7712ab2f9471e0f9767
junminlee06-gif <junminlee06@gmail.com>
2026-06-25 16:52:59 +0900

    Fix Vercel base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Odyssey/',
+  base: process.env.VERCEL ? '/' : '/Odyssey/',
   plugins: [react()]
 })
